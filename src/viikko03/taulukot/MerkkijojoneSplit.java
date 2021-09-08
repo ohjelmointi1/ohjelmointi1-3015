4ddf784e063cbecf8159ec76fdacbbe856a0f676
Teemu H <swd1tn002@users.noreply.github.com>
2021-09-08 11:35:54 +0300

    Viikon 3 esimerkkejä

---
+++ src/viikko03/taulukot/MerkkijojoneSplit.java
@@ -0,0 +1,29 @@
+package viikko03.taulukot;
+
+import java.util.Arrays;
+
+public class MerkkijojoneSplit {
+
+    public static void main(String[] args) {
+        String nykyhetki = "2021-10-13T11:21:00";
+        String[] pvmJaKello = nykyhetki.split("T");
+
+        System.out.println(pvmJaKello.length);
+
+        String pvm = pvmJaKello[0];
+        String kello = pvmJaKello[1];
+
+        String[] vuosiKkPv = pvm.split("-");
+        String[] hhMinSek = kello.split(":");
+
+        System.out.println(Arrays.toString(vuosiKkPv));
+        System.out.println(Arrays.toString(hhMinSek));
+
+        int vuosi = Integer.parseInt(vuosiKkPv[0]);
+        System.out.println("Seuraava vuosi " + (vuosi + 1));
+        int kk = Integer.parseInt(vuosiKkPv[1]);
+        int paiva = Integer.parseInt(vuosiKkPv[2]);
+
+        System.out.println(paiva + "." + kk + "." + vuosi);
+    }
+}
--- src/viikko03/taulukot/TaulukkoValmiillaArvoilla.java
+++ src/viikko03/taulukot/TaulukkoValmiillaArvoilla.java
@@ -3,9 +3,16 @@ package viikko03.taulukot;
 public class TaulukkoValmiillaArvoilla {
 
     public static void main(String[] args) {
-        String[] viikonpaivat = { "ma", "ti", "ke", "to", "pe", "la", "su" };
-        System.out.println(viikonpaivat.length);
 
-        System.out.println(viikonpaivat[3].toUpperCase());
+        String[] viikonpaivat = { "ma", "ti", "ke", "to", "pe", "la", "su" };
+
+        int keskiviikko = 2;
+        System.out.println(viikonpaivat[keskiviikko]);
+
+        System.out.println("Taulukon pituus: " + viikonpaivat.length);
+
+        for (int i = 0; i < viikonpaivat.length; i++) {
+            System.out.println(viikonpaivat[i].toUpperCase());
+        }
     }
 }
